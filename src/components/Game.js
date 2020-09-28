f964190e73eb863e3abcb0626ffe8f6aeb48b70e
Tristan Peng <pengt@catlin.edu>
2020-09-28 10:46:09 -0700

    Complete functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -14,8 +14,13 @@ const Game = () => {
     if (!winner) {
       const temp = JSON.parse(JSON.stringify(history));
       if (!temp[stepNumber][i]) {
-        temp.push(JSON.parse(JSON.stringify(temp[stepNumber])));
+        if (!temp.hasOwnProperty(stepNumber + 1)) {
+          temp.push(JSON.parse(JSON.stringify(temp[stepNumber])));
+        } else {
+          temp[stepNumber + 1] = JSON.parse(JSON.stringify(temp[stepNumber]));
+        }
         temp[stepNumber + 1][i] = next;
+        console.log(stepNumber, temp);
         setHistory(temp);
         setStepNumber(stepNumber + 1);
         setXIsNext(!xIsNext);
@@ -23,17 +28,26 @@ const Game = () => {
     }
   };
 
-  // console.log(history);
   const jump = step => {
-    const newHistory = history.slice(0, step + 1);
-    setHistory(newHistory);
     setStepNumber(step);
-    setXIsNext(!!(newHistory.length % 2));
+    setXIsNext(!(step % 2));
   };
 
   const result = () => winner ? `Winner: ${ winner }` : !history[stepNumber].some(square => square === null) ? `Tie Game` : `Next Player: ${ next }`;
 
-
+  const renderMoves = () => {
+    const buttons = [];
+    for (let i = 0; i < history.length; i++) {
+      buttons.push((
+        <span key={ `button-${ i }` }>
+          <button onClick={ () => jump(i) }>Go to { i === 0 ? 'Start' : `Move #${ i }` }</button>
+          <br />
+          <br />
+        </span>
+      ));
+    }
+    return buttons;
+  };
 
   return (
     <>
@@ -41,12 +55,12 @@ const Game = () => {
       <Board squares={ history[stepNumber] } onClick={ i => handleClick(i) } />
       <div className="info-wrapper">
         <div>
-          <button className="start" onClick={ () => jump(0) }>Go to Start</button>
+          { renderMoves() }
         </div>
         <h3>{ result() }</h3>
       </div>
     </>
-);
+  );
 };
 
 export default Game;
