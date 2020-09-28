ce9dc99204c9dd7506efa52bf81ed1c8c2bce4b5
Tristan Peng <pengt@catlin.edu>
2020-09-27 23:18:58 -0700

    Start working on functionallity

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,41 +3,45 @@ import { calculateWinner } from "../calculate_win";
 import Board from "./Board";
 
 const Game = () => {
-  const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null]);
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([[null, null, null, null, null, null, null, null, null]]);
 
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
   const next = xIsNext ? 'X' : 'O';
 
   const handleClick = i => {
     if (!winner) {
-      const temp = JSON.parse(JSON.stringify(board));
-      if (!temp[i]) {
-        temp[i] = next;
-        setBoard(temp);
+      const temp = JSON.parse(JSON.stringify(history));
+      if (!temp[stepNumber][i]) {
+        temp.push(JSON.parse(JSON.stringify(temp[stepNumber])));
+        temp[stepNumber + 1][i] = next;
+        setHistory(temp);
         setStepNumber(stepNumber + 1);
         setXIsNext(!xIsNext);
       }
     }
   };
 
-  const jumpToStart = () => {
-    const temp = JSON.parse(JSON.stringify(board));
-    setBoard(temp.fill(null));
-    setStepNumber(0);
-    setXIsNext(true);
+  // console.log(history);
+  const jump = step => {
+    const newHistory = history.slice(0, step + 1);
+    setHistory(newHistory);
+    setStepNumber(step);
+    setXIsNext(!!(newHistory.length % 2));
   };
 
-  const result = () => winner ? `Winner: ${ winner }` : !board.some(square => square === null) ? `Tie Game` : `Next Player: ${ next }`;
+  const result = () => winner ? `Winner: ${ winner }` : !history[stepNumber].some(square => square === null) ? `Tie Game` : `Next Player: ${ next }`;
+
+
 
   return (
     <>
       <h1 className="title">Tic Tac Toe</h1>
-      <Board squares={ board } onClick={ i => handleClick(i) } />
+      <Board squares={ history[stepNumber] } onClick={ i => handleClick(i) } />
       <div className="info-wrapper">
         <div>
-          <button className="start" onClick={ jumpToStart }>Go to Start</button>
+          <button className="start" onClick={ () => jump(0) }>Go to Start</button>
         </div>
         <h3>{ result() }</h3>
       </div>
